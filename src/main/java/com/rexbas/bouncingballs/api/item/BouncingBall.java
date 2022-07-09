e2b069d1b49d41350239350e3751fde16322ae1b
Rexbas <rexbasie@gmail.com>
2022-07-09 14:25:39 +0200

    Combine MultiBouncingBall and BouncingBall into 1 class

--- src/main/java/com/rexbas/bouncingballs/api/item/BouncingBall.java
+++ src/main/java/com/rexbas/bouncingballs/api/item/BouncingBall.java
@@ -16,6 +16,8 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 
 public class BouncingBall extends Item implements IBouncingBall {
 	
@@ -47,18 +49,27 @@ public class BouncingBall extends Item implements IBouncingBall {
 	
 	@Override
 	public boolean isValidRepairItem(ItemStack toRepair, ItemStack repair) {
-		return this.properties.repairItem != Items.AIR && repair.getItem() == this.properties.repairItem;
+		return properties.repairItem != Items.AIR && repair.getItem() == properties.repairItem;
 	}
 	
 	@Override
 	public boolean canBounce(LivingEntity entity) {
 		IBounceCapability cap = entity.getCapability(BounceCapabilityProvider.BOUNCE_CAPABILITY).orElse(null);
 		if (cap != null) {
-			return cap.getConsecutiveBounces() < 1 && entity.isOnGround() && !entity.isInWater() && !entity.isInLava();
+			if (properties.mustStartOnGround && cap.getConsecutiveBounces() == 0) {
+				return cap.getConsecutiveBounces() < properties.maxConsecutiveBounces && entity.isOnGround() && !entity.isInWater() && !entity.isInLava() && hasConsumptionItem(entity);
+			}
+			return cap.getConsecutiveBounces() < properties.maxConsecutiveBounces  && !entity.isInWater() && !entity.isInLava() && hasConsumptionItem(entity);
 		}
 		return false;
 	}
 	
+	/**
+	 * Add y-motion to the entity and reduce the consumption item if applicable.
+	 * 
+	 * @param entity  The entity to bounce.
+	 * @param motionY The y-motion to add.
+	 */
 	@Override
 	public void bounce(LivingEntity entity, float motionY) {
 		float yaw = entity.yRot;
@@ -71,17 +82,56 @@ public class BouncingBall extends Item implements IBouncingBall {
 		entity.getCapability(BounceCapabilityProvider.BOUNCE_CAPABILITY).ifPresent(cap -> {
 			cap.addBounce();
 		});
+		
+		if (properties.consumptionItem.getItem() != Items.AIR) {
+			entity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(itemHandler -> {
+				int slot = findConsumptionItemSlot(itemHandler);
+				if (slot != -1) {
+					itemHandler.extractItem(slot, 1, false);
+				}
+			});
+		}
 	}
 	
 	@Override
 	public float onFall(LivingEntity entity, ItemStack stack, float fallDistance) {
 		if (fallDistance > properties.rebounceHeight) {
-			bounce(entity, properties.upwardMotion);
+			float multiplier;
+			if (hasConsumptionItem(entity)) {
+				bounce(entity, properties.upwardMotion);
+				multiplier = properties.damageMultiplier;
+			} else {
+				bounce(entity, properties.upwardMotion / 2);
+				multiplier = properties.damageMultiplier * 2 > 1 ? 1 : properties.damageMultiplier * 2;
+			}
+
 			damageBall(entity, stack);
 			playBounceSound(entity.level, entity);
-			return properties.damageMultiplier;
+			return multiplier;
 		}
 		return 0f;
+	}
+	
+	protected boolean hasConsumptionItem(LivingEntity entity) {
+		if (properties.consumptionItem.getItem() == Items.AIR) {
+			return true;
+		}
+		
+		IItemHandler itemHandler = entity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).orElse(null);
+		if (itemHandler != null) {
+			return findConsumptionItemSlot(itemHandler) != -1;
+		}
+		return false;
+	}
+	
+	protected int findConsumptionItemSlot(IItemHandler itemHandler) {
+		for (int i = 0; i < itemHandler.getSlots(); ++i) {
+			ItemStack stack = itemHandler.getStackInSlot(i);
+			if (!stack.isEmpty() && stack.getItem() == properties.consumptionItem.getItem() && ItemStack.tagMatches(stack, properties.consumptionItem)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 	public void damageBall(LivingEntity entity, ItemStack stack) {
@@ -106,21 +156,32 @@ public class BouncingBall extends Item implements IBouncingBall {
 		protected float rebounceHeight;
 		protected float damageMultiplier;
 		
-		public Properties(int durability, Item repairItem, float forwardMotion, float upwardMotion, float rebounceHeight, float damageMultiplier) {
+		protected boolean mustStartOnGround;
+		protected int maxConsecutiveBounces;
+		protected ItemStack consumptionItem;
+		
+		public Properties(int durability, Item repairItem, float forwardMotion, float upwardMotion, float rebounceHeight, float damageMultiplier, boolean mustStartOnGround, int maxConsecutiveBounces, Item consumptionItem) {
 			this.durability = durability;
 			this.repairItem = repairItem;
 			this.forwardMotion = forwardMotion;
 			this.upwardMotion = upwardMotion;
 			this.rebounceHeight = rebounceHeight;
 			this.damageMultiplier = damageMultiplier;
+			this.mustStartOnGround = mustStartOnGround;
+			this.maxConsecutiveBounces = maxConsecutiveBounces;
+			this.consumptionItem = new ItemStack(consumptionItem);
 		}
 		
 		public Properties(float forwardMotion, float upwardMotion, float rebounceHeight, float damageMultiplier) {
-			this(0, Items.AIR, forwardMotion, upwardMotion, rebounceHeight, damageMultiplier);
+			this(0, Items.AIR, forwardMotion, upwardMotion, rebounceHeight, damageMultiplier, true, 1, Items.AIR);
+		}
+		
+		public Properties(float forwardMotion, float upwardMotion, float rebounceHeight, float damageMultiplier, boolean mustStartOnGround, int maxConsecutiveBounces, Item consumptionItem) {
+			this(0, Items.AIR, forwardMotion, upwardMotion, rebounceHeight, damageMultiplier, mustStartOnGround, maxConsecutiveBounces, consumptionItem);
 		}
 		
 		public Properties() {
-			this(100, Items.SLIME_BALL, 0.5f, 0.65f, 10f, 0.5f);
+			this(100, Items.SLIME_BALL, 0.5f, 0.65f, 10f, 0.5f, true, 1, Items.AIR);
 		}
 	}
 }
--- src/test/java/com/rexbas/bouncingballs/test/init/BouncingBallsTestItems.java
+++ src/test/java/com/rexbas/bouncingballs/test/init/BouncingBallsTestItems.java
@@ -1,7 +1,6 @@
 package com.rexbas.bouncingballs.test.init;
 
 import com.rexbas.bouncingballs.api.item.BouncingBall;
-import com.rexbas.bouncingballs.api.item.MultiBouncingBall;
 import com.rexbas.bouncingballs.test.BouncingBallsTest;
 
 import net.minecraft.item.Item;
@@ -17,5 +16,5 @@ public class BouncingBallsTestItems {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BouncingBallsTest.MODID);
 	
 	public static final RegistryObject<Item> NORMAL = ITEMS.register("normal", () -> new BouncingBall(new Item.Properties().tab(BouncingBallsTest.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> MULTI = ITEMS.register("multi", () -> new MultiBouncingBall(new Item.Properties().tab(BouncingBallsTest.ITEMGROUP), new BouncingBall.Properties(100, Items.DIAMOND, 0.5f, 0.65f, 12f, 0.3f), 5, Items.GUNPOWDER));
+	public static final RegistryObject<Item> MULTI = ITEMS.register("multi", () -> new BouncingBall(new Item.Properties().tab(BouncingBallsTest.ITEMGROUP), new BouncingBall.Properties(100, Items.DIAMOND, 0.5f, 0.65f, 12f, 0.3f, false, 5, Items.GUNPOWDER)));
 }
